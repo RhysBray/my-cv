74f15225fa6e919a9c66eb7cf04fbd284c3282f6
Rhys Bray <rhysabray@hotmail.com>
2019-05-23 20:55:46 +0100

    added fontawesome icons as links to my linkedin, github and email and made responsive

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import styles from "./header.module.scss";
 import rhys from "../../assets/images/square.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithubSquare } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
 
 export interface IProps {}
 
@@ -18,12 +21,37 @@ class Header extends React.Component<IProps, IState> {
     };
     this.state.isClicked ? (clicked = styles["moved-away"]) : (clicked = "");
     return (
-      <header onClick={handleClick} className={`${styles.header} ${clicked}`}>
-        <section className={styles.contents}>
-          <img className={styles.face} src={rhys} alt="rhys' face" />
-          <h1>Rhys Bray</h1>
-        </section>
-        <h2>- Welcome to my site -</h2>
+      <header className={`${styles.header} ${clicked}`}>
+        <div onClick={handleClick} className={styles["click-box"]} />
+        <div className={styles.contents}>
+          <img
+            onClick={handleClick}
+            className={styles.face}
+            src={rhys}
+            alt="rhys' face"
+          />
+          <h1 onClick={handleClick}>Rhys Bray</h1>
+          <h2 onClick={handleClick}>- Welcome to my site -</h2>
+          <section className={styles.links}>
+            <a
+              href="https://www.linkedin.com/in/rhys-bray-278a70147/"
+              target="_blank"
+              className={styles.icon}
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a
+              href="https://github.com/RhysBray"
+              target="_blank"
+              className={styles.icon}
+            >
+              <FontAwesomeIcon icon={faGithubSquare} />
+            </a>
+            <a href="mailto:rhysabray@hotmail.com" className={styles.icon}>
+              <FontAwesomeIcon icon={faEnvelopeSquare} />
+            </a>
+          </section>
+        </div>
       </header>
     );
   }

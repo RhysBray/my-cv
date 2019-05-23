ce9da34d0a5980c696c77b869c23bec7fb580cf3
Rhys Bray <rhysabray@hotmail.com>
2019-05-23 13:22:13 +0100

    header component responsive and has functionality to move when clicked

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,30 @@
 import * as React from "react";
 import styles from "./header.module.scss";
+import rhys from "../../assets/images/square.jpg";
+import { tsTypeAliasDeclaration } from "@babel/types";
 
 export interface IProps {}
 
-export interface IState {}
+export interface IState {
+  isClicked: boolean;
+}
 
 class Header extends React.Component<IProps, IState> {
+  state = { isClicked: false };
+
   public render() {
+    let clicked = "";
+    const handleClick = () => {
+      this.setState({ isClicked: !this.state.isClicked });
+    };
+    this.state.isClicked ? (clicked = styles["moved-away"]) : (clicked = "");
     return (
-      <header className={styles.header}>
-        <h1>Rhys Bray</h1>
+      <header onClick={handleClick} className={`${styles.header} ${clicked}`}>
+        <section className={styles.contents}>
+          <img className={styles.face} src={rhys} alt="rhys' face" />
+          <h1>Rhys Bray</h1>
+        </section>
+        <h2>- Welcome to my site -</h2>
       </header>
     );
   }
